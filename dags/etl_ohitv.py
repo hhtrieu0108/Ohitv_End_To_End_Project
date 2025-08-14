0d3f713e07cce4bda0e4af0b97b0d984735c33b8
Huynh Trieu <162791024+hhtrieu0108@users.noreply.github.com>
2025-08-14 11:55:53 +0700

    Update etl_ohitv.py

--- dags/etl_ohitv.py
+++ dags/etl_ohitv.py
@@ -5,17 +5,18 @@ from dags.group.load_to_db import load_tasks
 from datetime import datetime
 
 with DAG(
-        dag_id='ohitv_pipeline',
-        start_date=datetime(year=2024, 
-                            month=9, 
-                            day=28),
-        schedule_interval='30 9 * * *',
-        catchup=False) as dag:
+    dag_id='ohitv_pipeline',
+    start_date = datetime(
+        year=2024,
+        month=9, 
+        day=28
+    ),
+    schedule_interval='30 9 * * *',
+    catchup=False
+) as dag:
 
     crawl = crawl_tasks()
-
     processing = processing_tasks()
-
     load = load_tasks()
 
     crawl >> processing >> load
